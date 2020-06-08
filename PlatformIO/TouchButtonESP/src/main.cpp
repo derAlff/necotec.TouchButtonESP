a3e419a7726a0be6709687c8d35905fc8c6a8cc7
Oliver Alff (necotec) <o.alff@necotec.org>
2020-06-08 21:19:41 +0200

    Version 1.0 . MQTT is running. Wifi connection is ok. Set hostname an uniqe, because created from MAC-address. At version 1.0 only static MQTT-topics. At the moment work only one topic.

--- PlatformIO/TouchButtonESP/src/main.cpp
+++ PlatformIO/TouchButtonESP/src/main.cpp
@@ -5,45 +5,54 @@
 #include <Arduino.h>
 #include <string.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include "mqtt.h"
 
 #define TOUCHTASTER   16 // define pin 16 (D0) as pin for the first touchpad
 #define TOUCHTASTER2  14 // define pin 14 (D5) as pin for the second touchpad
 #define TOUCHTASTER3  12 // define pin 12 (D6) as pin for the third touchpad
 #define TOUCHTASTER4  13 // define pin 13 (D7) as pin for the fourth touchpad
 
-#define TEST_LED      4 // define pin 4 (D2) as pin for the touchpad
+#define TEST_LED      4  // define pin 4 (D2) as pin for the touchpad
 
-String MqttFolder = ""; // the MQTT path
-String MqttServerAddress = ""; // the MQTT Server
+#define MSG_BUFFER_SIZE   50
+String DeviceWifiHostname = "";                   // the hostname for this device
+const String WifiSsid = "your wifi ssid";           // WIFI SSID
+const String WifiPassword = "your wifi password";   // WIFI password
+String MacAddress = "";
 
-String DeviceWifiHostname = "Testdevice";  // the hostname for this device
+// MQTT
+const char* MqttTopic = "Zwischenraum/testTaster";    // the MQTT path
+const char* MqttServerAddress = "192.168.10.34";      // the MQTT Server !!use ip address!!
+uint16_t MqttPort = 1883;                             // the port of MQTT Server
+WiFiClient WifiClient;
+PubSubClient MqttClient(WifiClient);
+unsigned long lastMsg = 0;
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
 
-const String WifiSsid = "";  // WIFI SSID
-const String WifiPassword = ""; // WIFI password
+const char* PublishTopic1 = "trigger"; 
+const char* PublishTopic2; 
+const char* PublishTopic3; 
+const char* PublishTopic4;
+const char* SubscribeTopic1;
+const char* SubscribeTopic2; 
+const char* SubscribeTopic3;
+const char* SubscribeTopic4; 
 
-bool RunCode = true;
-
+bool RunCode = true;  //  if no connection to wifi, then don't run the program
+ 
 
 bool led = false; // boolean for the led state
 
-// function for led toggle
-void toggleLed(bool state)
+/* ****************** Functions ****************** */
+
+// setup the wifi
+void setup_wifi()
 {
-  if(state)
-  {
-    digitalWrite(TEST_LED, HIGH);
-  }
-  else
-  {
-    digitalWrite(TEST_LED, LOW);
-  }
+  delay(10);
 
-  Serial.print("State of the LED: ");
-  Serial.println(state);
-}
-
-void setup() {
-  // connect to WIFI
   if((WifiSsid != emptyString || WifiSsid != NULL) && (WifiPassword != emptyString || WifiPassword != NULL))
   {
     uint8_t wifiConnectCounter = 0;
@@ -54,8 +63,6 @@ void setup() {
     Serial.println(WifiPassword);
 
     // connect to Wifi and take serial outputs
-    WiFi.hostname(DeviceWifiHostname);
-    Serial.println("Set Hostname to: " + DeviceWifiHostname);
     WiFi.begin(WifiSsid, WifiPassword);
     Serial.print("Connect to Wifi " + WifiSsid);
     Serial.print(".");
@@ -74,11 +81,82 @@ void setup() {
     }
     if(RunCode){
       Serial.println("Wifi connected!");
+      MacAddress = WiFi.macAddress();
+      
+      // Change Hostname
+      String tempMacAddress = "";
+      char tempChar;
+      for(uint8_t i = 0; i < MacAddress.length(); i++)
+      {
+        tempChar = MacAddress[i];
+
+        if(tempChar != ':')
+        {
+          tempMacAddress += tempChar;
+        }
+      }
+
+      // Set hostname and print on serial console
+      DeviceWifiHostname = "NCT-ESP-" + tempMacAddress;
+      WiFi.hostname(DeviceWifiHostname);
+      Serial.println("Set Hostname to: " + DeviceWifiHostname);
+      Serial.println("MAC-Address of device: " + MacAddress);
     }
     else{
       Serial.println("Can not connect to Wifi! Timeout! Please check the connection options!");
     }
   }
+}
+
+// setup MQTT
+void setup_mqtt()
+{
+
+}
+
+// MQTT reconnect
+void mqttReconnect()
+{
+  while(!MqttClient.connected())
+  {
+    Serial.print("Reconnect to MQTT-broker...");
+    if(MqttClient.connect("EspClient"))
+    {
+      Serial.print("Connected to MQTT-broker.");
+    }
+    else{
+      Serial.print("Failed to connect. State: ");
+      Serial.println(MqttClient.state());
+      Serial.print("Trying reconnect in 5 seconds.");
+      delay(5000);
+    }
+  }
+}
+
+// function for led toggle
+void toggleLed(bool state)
+{
+  if(state)
+  {
+    digitalWrite(TEST_LED, HIGH);
+    MqttClient.publish(MqttTopic,PublishTopic1);
+  }
+  else
+  {
+    digitalWrite(TEST_LED, LOW);
+    MqttClient.publish(MqttTopic,PublishTopic1);
+  }
+
+  Serial.print("State of the LED: ");
+  Serial.println(state);
+}
+
+/* **************** End functions **************** */
+
+void setup() {
+  // begin a serial connection for output
+  Serial.begin(9600);
+  Serial.println("Program started.");
 
   // set up the pins
   pinMode(TOUCHTASTER, INPUT);
@@ -89,15 +167,23 @@ void setup() {
 
   // toggle of the led by starting
   digitalWrite(TEST_LED, LOW);
-  
-  // begin a serial connection for output
-  Serial.begin(9600);
-  Serial.println("Program started.");
+
+  // connect to WIFI
+  setup_wifi();
+  MqttClient.setServer(MqttServerAddress, MqttPort);
 }
 
 void loop() {
   if(RunCode)
   {
+    // check MQTT connection
+    if(!MqttClient.connected())
+    {
+      mqttReconnect();
+      Serial.println("State of \"MqttClient\"" + MqttClient.connected());
+    }
+    MqttClient.loop();
+
     // check the state of the pin and toggle
     if(digitalRead(TOUCHTASTER))
     {
@@ -119,12 +205,11 @@ void loop() {
       Serial.println();
       toggleLed(led); // and now, toggle the led
     }
-    else{
-      Serial.println("Wifi is not connected. The code on this device do not running! Please check the options!");
-      delay(1000);
-    }
   }
-
-  
+  else
+  {
+    Serial.println("Wifi is not connected. The code on this device do not running! Please check the options!");
+    delay(1000);
+  }
 }
 
---
+++ PlatformIO/TouchButtonESP/src/mqtt.h
@@ -0,0 +1,10 @@
+#ifndef MQTT
+#define MQTT
+
+#include <Arduino.h>
+
+void serialPrintMqtt(){
+    Serial.println("from the \"mqtt.cpp\"");
+}
+
+#endif
